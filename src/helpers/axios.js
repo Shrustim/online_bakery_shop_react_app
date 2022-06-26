b617d4e3c7966c82a706ad25f7e251b2e6b4dbee
shrushti mane <shrustimanexyz@gmail.com>
2022-06-26 17:34:04 +0530

    changes

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 export default axios.create({
-    // baseURL:'https://temp-app-windowshop.herokuapp.com/'
-    baseURL:"http://localhost:3000/"
+    baseURL:'https://temp-app-windowshop.herokuapp.com/'
+    // baseURL:"http://localhost:3000/"
 });
 
 const token = JSON.parse(localStorage.getItem('token'));
 export const WithTokenApi = axios.create({
-    // baseURL:'https://temp-app-windowshop.herokuapp.com/',
-    baseURL:"http://localhost:3000/"
+    baseURL:'https://temp-app-windowshop.herokuapp.com/',
+    // baseURL:"http://localhost:3000/"
      // headers: {"Authorization" : `Bearer ${token}`} 
 });
 
